488f03c9bd2052c91a9e421e403697ed83d2ceac
HyungJun Lee <mirae1jun@gmail.com>
2025-08-26 03:25:33 +0900

    feat: 하단 바 아이콘 적용
    선택/미선택 시에 따른 아이콘 변화 적용.
    일단 여행자 역할로서의 환경을 기준으로 둠

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import { Platform } from 'react-native';
 
 import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+// symbols
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -31,45 +34,60 @@ export default function TabLayout() {
         name='index'
         options={{
           title: '홈',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='house.fill' color={color} />
-          ),
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <Ionicons name='home' size={28} color={color} />
+            ) : (
+              <Ionicons name='home-outline' size={28} color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name='explore'
         options={{
           title: '검색',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='paperplane.fill' color={color} />
-          ),
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <Ionicons name='search' size={28} color={color} />
+            ) : (
+              <Ionicons name='search-outline' size={28} color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name='travel'
         options={{
           title: '여행',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='paperplane.fill' color={color} />
-          ),
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <MaterialIcons name='flight' size={28} color={color} />
+            ) : (
+              <MaterialIcons name='flight-takeoff' size={28} color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name='wishlist'
         options={{
           title: '찜',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='paperplane.fill' color={color} />
-          ),
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <MaterialIcons name='favorite' size={28} color={color} />
+            ) : (
+              <MaterialIcons name='favorite-border' size={28} color={color} />
+            ),
         }}
       />
       <Tabs.Screen
         name='profile'
         options={{
           title: '프로필',
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name='paperplane.fill' color={color} />
-          ),
+          tabBarIcon: ({ focused, color }) =>
+            focused ? (
+              <MaterialIcons name='person' size={28} color={color} />
+            ) : (
+              <MaterialIcons name='person-outline' size={28} color={color} />
+            ),
         }}
       />
     </Tabs>
