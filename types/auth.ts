3b2c8bd4eb9fbca64e13015a650f58f65badb38c
newfangledmindset <newfangledmindset@gmail.com>
2025-09-09 03:05:09 +0900

    [#5]feat: API 명세서를 바탕으로 한 interface 작성

---
+++ types/auth.ts
@@ -0,0 +1,19 @@
+import APIResponse from "./apiResponse";
+
+interface LoginRequest {
+  socialAccessToken: string;
+}
+
+interface LoginResponse extends APIResponse<LoginData> {}
+  
+}
+
+interface LogoutRequest {
+  accessToken: string;
+}
+
+interface LoginData {
+  accessToken: string;
+  refreshToken: string;
+  isNewUser: boolean;
+}
