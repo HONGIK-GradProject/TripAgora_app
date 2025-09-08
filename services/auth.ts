d55d3bb8ae240fa7ba4b68827f98d5b3bebb9f7b
newfangledmindset <newfangledmindset@gmail.com>
2025-09-09 03:04:38 +0900

    [#5]feat: secure store를 이용한 token 관리 기능 추가

---
+++ services/auth.ts
@@ -0,0 +1,32 @@
+import * as SecureStore from 'expo-secure-store';
+
+const saveTokens = async (accessToken: string, refreshToken: string) => {
+  try {
+    await SecureStore.setItemAsync('accessToken', accessToken);
+    await SecureStore.setItemAsync('refreshToken', refreshToken);
+  } catch (error) {
+    console.error('Error saving tokens:', error); 
+  }
+}
+
+const clearTokens = async () => {
+  try {
+    await SecureStore.deleteItemAsync('accessToken');
+    await SecureStore.deleteItemAsync('refreshToken');
+  } catch (error) {
+    console.error('Error clearing tokens:', error);
+  }
+}
+
+const getTokens = async (): Promise<{ accessToken: string | null; refreshToken: string | null }> => {
+  try {
+    const accessToken = await SecureStore.getItemAsync('accessToken');
+    const refreshToken = await SecureStore.getItemAsync('refreshToken');
+    return { accessToken, refreshToken };
+  } catch (error) {
+    console.error('Error retrieving tokens:', error);
+    return { accessToken: null, refreshToken: null };
+  }
+}
+
+export { clearTokens, getTokens, saveTokens };
