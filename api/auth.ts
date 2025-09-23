dab1e4e7d5168c5fa2e66034bf395e33c5b46e74
newfangledmindset <newfangledmindset@gmail.com>
2025-09-23 15:41:34 +0900

    [#28]docs: 주석 수정

--- api/auth.ts
+++ api/auth.ts
@@ -45,7 +45,7 @@ export const signIn = async (
 };
 
 /**
- * 서버에서 로그아웃을 처리하고, 저장된 토큰을 삭제합니다.
+ * 서버에서 로그아웃을 처리하합니다.
  * @returns {Promise<void>}
  */
 export const signOut = async (): Promise<void> => {
