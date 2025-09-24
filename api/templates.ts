c8c7c759d08552162e071067c60adf09dbeae6f9
HyungJun Lee <mirae1jun@gmail.com>
2025-09-25 03:22:02 +0900

    revert: api 관련 함수 제거
    이슈 #29 에서 따로 작성될 예정인 부분을 삭제합니다.

--- api/templates.ts
+++ api/templates.ts
@@ -2,19 +2,3 @@
  * @file 템플릿 관련 API 함수를 제공합니다. (템플릿 생성, 템플릿 조회, 템플릿 수정, 템플릿 삭제)
  * @module api/templates
  */
-
-import {
-  CreateTemplateRequest,
-  CreateTemplateResponse,
-} from '../types/templates';
-import apiClient from './client';
-
-export const createTemplate = async (
-  request: CreateTemplateRequest
-): Promise<CreateTemplateResponse> => {
-  const response = await apiClient.post<CreateTemplateResponse>(
-    '/templates',
-    request
-  );
-  return response.data;
-};
--- types/templates.ts
+++ types/templates.ts
@@ -1,15 +1,5 @@
 // import APIResponse from "./apiResponse";
 
-import APIResponse from './apiResponse';
+interface CreateTemplateRequest {}
 
-interface CreateTemplateRequest {
-  accessToken: string;
-}
-
-interface CreateTemplateResponse extends APIResponse<CreateTemplateData> {}
-
-interface CreateTemplateData {
-  templateId: number;
-}
-
-export { CreateTemplateData, CreateTemplateRequest, CreateTemplateResponse };
+export { CreateTemplateRequest };
