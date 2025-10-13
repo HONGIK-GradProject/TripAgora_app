d2229d13af753d5bafbabc923f7a02a91e4acde0
newfangledmindset <newfangledmindset@gmail.com>
2025-10-13 15:45:26 +0900

    [#35]fix: 특정 상황에서 세션 리스트가 보이지 않던 문제 수정

--- hooks/sessions/useSessionList.ts
+++ hooks/sessions/useSessionList.ts
@@ -28,6 +28,9 @@ export const useSessionList = () => {
       setError(null);
       console.log('Calling page ', pageToLoad);
 
+      // statuses가 비었을 때 기본 statuses를 설정합니다.
+      if (!statuses) statuses = ['RECRUITING', 'RECRUITMENT_CLOSED', 'IN_PROGRESS'];
+
       try {
         const response = await getSessionList(statuses, pageToLoad);
 
