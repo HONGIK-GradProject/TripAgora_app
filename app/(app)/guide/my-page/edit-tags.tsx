055387098d258a862cf05a8106eff4e40fb8ffc5
newfangledmindset <newfangledmindset@gmail.com>
2025-11-08 01:17:24 +0900

    [#70]feat: SafeAreaView 적용

--- app/(app)/guide/my-page/edit-tags.tsx
+++ app/(app)/guide/my-page/edit-tags.tsx
@@ -1,7 +1,12 @@
 import MyTagsScreen from "@/components/common/MyTagsScreen";
+import CustomSafeAreaView from "@/components/CustomSafeAreaView";
 
-const TravelerMyPageEditTagsScreen = () => {
-  return <MyTagsScreen />
+const GuideMyPageEditTagsScreen = () => {
+  return (
+    <CustomSafeAreaView>
+      <MyTagsScreen />
+    </CustomSafeAreaView>
+  );
 }
 
-export default TravelerMyPageEditTagsScreen;
+export default GuideMyPageEditTagsScreen;
--- app/(app)/traveler/my-page/edit-tags.tsx
+++ app/(app)/traveler/my-page/edit-tags.tsx
@@ -1,7 +1,12 @@
 import MyTagsScreen from "@/components/common/MyTagsScreen";
+import CustomSafeAreaView from "@/components/CustomSafeAreaView";
 
 const TravelerMyPageEditTagsScreen = () => {
-  return <MyTagsScreen />
+  return (
+    <CustomSafeAreaView>
+      <MyTagsScreen />
+    </CustomSafeAreaView>
+  );
 }
 
 export default TravelerMyPageEditTagsScreen;
