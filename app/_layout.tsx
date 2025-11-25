36309f51a8930f44afc0cc14a09c6b642cebc50a
newfangledmindset <newfangledmindset@gmail.com>
2025-11-26 01:05:58 +0900

    [#97]feat: 푸시 알림 Foreground / Background 설정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ import {
   ThemeProvider,
 } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
@@ -13,8 +13,22 @@ import '../global.css';
 
 import { AuthProvider } from '@/contexts/AuthContext';
 import { StompProvider } from '@/contexts/StompContext';
+import { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Toast from 'react-native-toast-message';
+
+import * as Notifications from 'expo-notifications';
+
+// 앱이 포어그라운드에서 실행 중일 때 알림을 어떻게 처리할지 설정합니다.
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+  }),
+});
 
 SplashScreen.setOptions({
   duration: 1000,
@@ -26,6 +40,28 @@ export default function RootLayout() {
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+
+  const router = useRouter();
+
+  const lastNotificationResponse = Notifications.useLastNotificationResponse();
+
+  useEffect(() => {
+    if (lastNotificationResponse && lastNotificationResponse.actionIdentifier === Notifications.DEFAULT_ACTION_IDENTIFIER) {
+      // TODO: url을 실제 deep link property로 변경
+      const { url, type } = lastNotificationResponse.notification.request.content.data;
+      if (url) {
+        router.push(url as any);
+      }
+    }
+  }, [lastNotificationResponse]);
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(notification => {
+      // TODO: Add new foreground task
+    });
+
+    return () => subscription.remove();
+  }, []);
 
   if (!loaded) {
     return null;
